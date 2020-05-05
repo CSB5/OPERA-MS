392c44313cb44ea904537e800042df447727b545
TONG Chengxuan <tongcx@n111.default.domain>
2020-05-05 12:55:35 +0800

    update 8

--- tools_opera_ms/MaxBin-2.2.4/auxiliary/hmmer-3.1b1/easel/esl_composition.h
+++
@@ -1,4 +0,0 @@
-extern int  esl_composition_BL62(double *f);
-extern int  esl_composition_WAG (double *f);
-extern int  esl_composition_SW34(double *f);
-extern int  esl_composition_SW50(double *f);
